1930729ce219b8ca7b987e1c139aa7980ff34581
TitanEric <chenyihuang001@gmail.com>
2018-07-11 22:54:14 +0800

    Add the DP solution to knapsack in DO (51/60)

--- Discrete Optimization/knapsack/solver.py
+++ Discrete Optimization/knapsack/solver.py
@@ -23,10 +23,13 @@ def solve_it(input_data):
         parts = line.split()
         items.append(Item(i-1, int(parts[0]), int(parts[1])))
 
+    if item_count <= 200:
+        obj, taken = dynimic_programming(items, capacity)
+    else:
+        obj, taken = greedy(items, capacity)
 
-    obj, taken = greedy(items, capacity)
     # prepare the solution in the specified output format
-    output_data = "{} {}\n".format(obj, 0)
+    output_data = "{} {}\n".format(obj, 1)
     output_data += ' '.join(map(str, taken))
     return output_data
 
@@ -49,6 +52,29 @@ def greedy(items, capacity):
 
     return obj, taken
 
+
+def dynimic_programming(items, capacity):
+    table = [[0 for _ in range(capacity+1)] for _ in range(len(items)+1)]
+    for i, item in zip(range(1, len(items)+1), items):
+        for k in range(1, capacity+1):
+            table[i][k] = max(table[i-1][k], item.value + table[i-1][k-item.weight]) \
+                            if item.weight <= k else table[i-1][k]
+    obj = table[len(items)][capacity]
+    taken = _find_DP_taken(items, table, capacity, obj)
+    return obj, taken
+
+def _find_DP_taken(items: list, table, capacity, obj):
+    taken = [0 for _ in range(len(items))]
+    val = obj
+    k = capacity
+    for i, item in zip(reversed(range(1, len(items)+1)), reversed(items)):
+        if table[i-1][k] == val:
+            continue
+        else:
+            taken[item.index] = 1
+            k -= item.weight
+            val = table[i-1][k]
+    return taken
 
 if __name__ == '__main__':
     import sys
